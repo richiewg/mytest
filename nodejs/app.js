58ae445650c2af390ffa546c3cd3ffcfba3f080b
richiewg <154223418@qq.com>
2017-02-04 17:25:06 +0800

    Add files via upload

--- nodejs/app.js
+++ nodejs/app.js
@@ -15,155 +15,163 @@ function onRequest(request,response)
     var pathname = url.parse(request.url).pathname;
     if(pathname=="/")    //访问表单页面
     {
-
-        var ObjBcu2 = new bcu();
-        var args2 = [];
-        args2.push("query");
-        args2.push("a");
-        ObjBcu2.chaincode.query(args2, function (err, res2, body) {
-            //var jsondataz = JSON.stringify(body.result.message);
-            //console.log(JSON.stringify(body));
-//            console.log(JSON.stringify(body.result));
-            _moneyJ =JSON.stringify(body.result.message)
-            console.log(_moneyJ)
-            var ObjBcu3 = new bcu();
-            var args3 = [];
-            args3.push("query");
-            args3.push("b");
-            ObjBcu3.chaincode.query(args3, function (err, res3, body) {
-                //var jsondataz = JSON.stringify(body.result.message);
-                _moneyY =JSON.stringify(body.result.message)
-                console.log(_moneyY)
-                response.writeHead(200,{"Content-Type":"text/html"});
-                fs.readFile("index.html","utf-8",function(e,data){
-                data = data + "</br></br><fieldset><legend>甲方的账本现为</legend>甲方："+ _moneyJ+" </fieldset></br></br><fieldset><legend>乙方的账本现为</legend>乙方："+ _moneyY+"</fieldset></div></body></html>"
-                response.write(data);
-                response.end();
-                });
-            });
-        });
+        response.writeHead(200,{"Content-Type":"text/html"});
+        fs.readFile("index.html","utf-8",function(e,data){
+        		response.write(data);
+        		response.end();
+		});
     }
     else if(pathname=="/b")    //处理post方式请求
-    {   
+	{
+        response.writeHead(200,{"Content-Type":"text/html"});
+        fs.readFile("b.html","utf-8",function(e,data){
+        		response.write(data);
+        		response.end();
+		});		
+	}
+    else if(pathname=="/a")    //处理post方式请求
+	{
+        response.writeHead(200,{"Content-Type":"text/html"});
+        fs.readFile("a.html","utf-8",function(e,data){
+        		response.write(data);
+        		response.end();
+		});		
+	}	
+    else if(pathname=="/c")    //处理post方式请求
+	{
+        response.writeHead(200,{"Content-Type":"text/html"});
+        fs.readFile("c.html","utf-8",function(e,data){
+        		response.write(data);
+        		response.end();
+		});		
+	}	
+    else if(pathname=="/RegistdQuery")    //处理post方式请求
+	{
+        response.writeHead(200,{"Content-Type":"text/html"});
+        fs.readFile("RegistdQuery.html","utf-8",function(e,data){
+        		response.write(data);
+        		response.end();
+		});		
+	}		
+    else if(pathname=="/regist")    //处理post方式请求
+	{
         var urlstr="";
         request.addListener("data",function(postdata){
             urlstr+=postdata;    //接收到的表单数据字符串，这里可以用两种方法将UTF-8编码转换为中文
             var jsondata = qs.parse(urlstr);        //转换成json对象
             var decodedata = decodeURIComponent(urlstr);        //对表单数据进行解码
-            console.log(jsondata.outperson)
-            console.log(jsondata.inperson)
-            console.log(jsondata.money)
-            console.log(urlstr);
-            console.log('toJson',JSON.stringify(jsondata));
-            console.log(decodedata);
             urlstr = decodedata;
-            if (jsondata.money != '' && jsondata.money != 0) {
-                console.log("###### invoke start #########################")
+            if (jsondata.Shanghu != '' && jsondata.Identity != '' && jsondata.ServerAddress != '' ) {
                 var ObjBcu = new bcu();
                 var args = [];
-                args.push("invoke")
-                args.push(jsondata.inperson);
-                args.push(jsondata.outperson);
-                args.push(jsondata.money);
-                _moneyZ= _moneyJ
-                console.log('moneyj='+ _moneyJ)
-                console.log('moneyz='+ _moneyZ)
-                //arges = request.body.params.args;
-                ObjBcu.chaincode.invoke(args, function (err, res1, body) {
-                    checkab()
-                    function checkab(){
-                        console.log("111")
-                        var ObjBcu2 = new bcu();
-                        var args2 = [];
-                        args2.push("query");
-                        args2.push("a");
-                        var ObjBcu3 = new bcu();
-                        var args3 = [];
-                        args3.push("query");
-                        args3.push("b");
-                        console.log("222")
-                        ObjBcu2.chaincode.query(args2, function (err, res2, body) {
-                            //var jsondataz = JSON.stringify(body.result.message);
-                            _moneyJ =JSON.stringify(body.result.message)
-                            console.log("333")                            
-                            // console.log(_moneyJ)
-                            ObjBcu3.chaincode.query(args3, function (err, res3, body) {
-                                //var jsondataz = JSON.stringify(body.result.message);
-                                _moneyY =JSON.stringify(body.result.message);
-                                console.log('moneyj='+ _moneyJ)
-                                console.log('moneyz='+ _moneyZ)
-                                if (_moneyJ== _moneyZ){
-                                    console.log("equal")
-                                    checkab()
-                                }else{
-                                    console.log("not equal")
-                                    response.writeHead(200,{"Content-Type":"text/html"});
-                                    fs.readFile("index.html","utf-8",function(e,data){
-                                    data = data + "</br></br><fieldset><legend>甲方的账本现为</legend>甲方："+ _moneyJ+" </fieldset></br></br><fieldset><legend>乙方的账本现为</legend>乙方："+ _moneyY+"</fieldset></div></body></html>";
-                                    response.write(data);
-                                    response.end();
-                                    });
-                                    return
-                                }
-                                // response.writeHead(200,{"Content-Type":"text/html"});
-                                // fs.readFile("index.html","utf-8",function(e,data){
-                                // data = data + "</br></br><fieldset><legend>甲方的账本现为</legend>甲方："+ _moneyJ+" </fieldset></br></br><fieldset><legend>乙方的账本现为</legend>乙方："+ _moneyY+"</fieldset></div></body></html>";
-                                // response.write(data);
-                                // response.end();
-                                // });
-                            });
-                        });
-                    };
-                });
-            }else{
-                var ObjBcu2 = new bcu();
-                var args2 = [];
-                args2.push("query");
-                args2.push("a");
-                ObjBcu2.chaincode.query(args2, function (err, res2, body) {
-                    //var jsondataz = JSON.stringify(body.result.message);
-                    _moneyJ =JSON.stringify(body.result.message)
-                    console.log(_moneyJ)
-                    var ObjBcu3 = new bcu();
-                    var args3 = [];
-                    args3.push("query");
-                    args3.push("b");
-                    ObjBcu3.chaincode.query(args3, function (err, res3, body) {
-                        //var jsondataz = JSON.stringify(body.result.message);
-                        _moneyY =JSON.stringify(body.result.message)
-                        console.log(_moneyY)
-                        response.writeHead(200,{"Content-Type":"text/html"});
-                        fs.readFile("index.html","utf-8",function(e,data){
-                        data = data + "</br></br><fieldset><legend>甲方的账本现为</legend>甲方："+ _moneyJ+" </fieldset></br></br><fieldset><legend>乙方的账本现为</legend>乙方："+ _moneyY+"</fieldset></div></body></html>"
-                        response.write(data);
-                        response.end();
-                        });
-                    });
+                args.push("InvokeRegist")
+                args.push(jsondata.Identity);
+                args.push(jsondata.Shanghu);				
+                args.push(jsondata.ServerAddress);
+			    ObjBcu.chaincode.invoke(args, function (err, res, body) {
+					if (body.result.status == "OK") {
+				        response.writeHead(200,{"Content-Type":"text/html"});
+				        fs.readFile("success.html","utf-8",function(e,data){
+				        		response.write(data);
+				        		response.end();
+						});		
+					};	
+				});
+			};
+		});
+    }	
+    else if(pathname=="/delete")    //处理post方式请求
+	{
+        var urlstr="";
+        request.addListener("data",function(postdata){
+            urlstr+=postdata;    //接收到的表单数据字符串，这里可以用两种方法将UTF-8编码转换为中文
+            var jsondata = qs.parse(urlstr);        //转换成json对象
+            var decodedata = decodeURIComponent(urlstr);        //对表单数据进行解码
+            urlstr = decodedata;
+            if (jsondata.Shanghu != '' && jsondata.Identity != '' && jsondata.ServerAddress != '' ) {
+                var ObjBcu = new bcu();
+                var args = [];
+                args.push("InvokeDelete")
+                args.push(jsondata.Identity);				
+                args.push(jsondata.Shanghu);
+			    ObjBcu.chaincode.invoke(args, function (err, res, body) {
+					if (body.result.status == "OK") {
+				        response.writeHead(200,{"Content-Type":"text/html"});
+				        fs.readFile("success.html","utf-8",function(e,data){
+				        		response.write(data);
+				        		response.end();
+						});		
+					};	
+				});
+			};
+		});
+    }		
+    else if(pathname=="/QueryDetails")    //处理post方式请求
+	{
+        var urlstr="";
+        request.addListener("data",function(postdata){
+            urlstr+=postdata;    //接收到的表单数据字符串，这里可以用两种方法将UTF-8编码转换为中文
+			console.log(urlstr)
+            var jsondata = qs.parse(urlstr);        //转换成json对象
+            var decodedata = decodeURIComponent(urlstr);        //对表单数据进行解码
+            urlstr = decodedata;
+            if (jsondata.Identity != '') {
+                var ObjBcu = new bcu();
+                var args = [];
+                args.push("QueryDetails")
+                args.push(jsondata.Identity);
+			    ObjBcu.chaincode.query(args, function (err, res, body) {
+					if (body.result.status == "OK") {
 
-                });
-            }
-        });
+				        response.writeHead(200,{"Content-Type":"text/html"});
+				        fs.readFile("QueryDetails.html","utf-8",function(e,data){
+							//var jsonx  = JSON.parse(body.result.message);
+							console.log(body.result.message)					
+							//console.log(jsonx.userName)							
+							//data = data + "<td>" + jsonx.userName+ "</td><td>" + jsonx.identity + "</td><td>" + jsonx.money + "</td><td>" + jsonx.txtype + "</td><td>"  + jsonx.date + "</td><td>"    
+							//data = data + "</td></tr></tbody></table></div></body>	"
+							data = data +"<td>" + jsondata.Identity  + "</td><td>" + body.result.message +"</td>"	
+							data = data + "</tr></tbody></table></div></body>"
+							response.write(data);						
+				        		response.end();
+						});		
+					};	
+				});
+			};
+		});
     }
-}
-function querya(){
-    var ObjBcu2 = new bcu();
-    var args2 = [];
-    args2.push("query");
-    args2.push("a");
-    ObjBcu2.chaincode.query(args2, function (err, res2, body) {
-        //var jsondataz = JSON.stringify(body.result.message);
-        _moneyJ =JSON.stringify(body.result.message);
-    });
-    console.log('async moneyj'+ _moneyJ)    
-}
-function queryb(){
-    var ObjBcu2 = new bcu();
-    var args2 = [];
-    args2.push("query");
-    args2.push("b");
-    ObjBcu2.chaincode.query(args2, function (err, res2, body) {
-        //var jsondataz = JSON.stringify(body.result.message);
-        _moneyY =JSON.stringify(body.result.message)
-    });
-    console.log('async moneyj'+ _moneyY)        
+    else if(pathname=="/QueryRegisted")    //处理post方式请求
+	{
+        var urlstr="";
+        request.addListener("data",function(postdata){
+            urlstr+=postdata;    //接收到的表单数据字符串，这里可以用两种方法将UTF-8编码转换为中文
+			console.log(urlstr)
+            var jsondata = qs.parse(urlstr);        //转换成json对象
+            var decodedata = decodeURIComponent(urlstr);        //对表单数据进行解码
+            urlstr = decodedata;
+            if (jsondata.Identity != '') {
+                var ObjBcu = new bcu();
+                var args = [];
+                args.push("QueryRegisted")
+                args.push(jsondata.Identity);
+			    ObjBcu.chaincode.query(args, function (err, res, body) {
+					if (body.result.status == "OK") {
+
+				        response.writeHead(200,{"Content-Type":"text/html"});
+				        fs.readFile("QueryRegisted.html","utf-8",function(e,data){
+//							var jsonx  = JSON.parse(body.result.message);
+//							console.log(jsonx.userName)							
+//							data = data + "<td>" + jsonx.userName+ "</td><td>" + jsonx.identity + "</td><td>" + jsonx.address + "</td><td>"    
+//							data = data + "</td></tr></tbody></table></div></body>	"
+//				        		response.write(data);	
+							data = data +"<td>" + jsondata.Identity  + "</td><td>" + body.result.message +"</td>"	
+							data = data + "</tr></tbody></table></div></body>"
+							response.write(data);									
+				        		response.end();
+						});		
+					};	
+				});
+			};
+		});
+    };			
 }
